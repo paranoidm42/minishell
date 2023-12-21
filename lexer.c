a2433a2b9e64165307c573d9c817ec4a7f0d98b9
paranoidm42 <cemlur125@gmail.com>
2023-12-21 07:08:44 +0300

    42

--- lexer.c
+++ lexer.c
@@ -1,20 +1,85 @@
 #include "minishell.h"
-
-
-void lexer_main(t_data *data)
+int	ft_sstrlen(char **s)
 {
-	char **swap;
+	int	lenght;
+
+	lenght = 0;
+	if (!s)
+		return (0);
+	lenght = 0;
+	while (s[lenght])
+		lenght++;
+	return (lenght);
+}
+
+static int ft_count_word(char *str)
+{
     int i = 0;
-    swap = ft_split(data->cmd,' ');
-    while (swap[i])
+    int pass = 0;
+    char test;
+    while (str[i])
     {
-        printf("%s\n",swap[i]);
+        test = str[i];
+        if (str[i] == '\'' || str[i] == '\"')
+        {
+            pass++;
+            while (str[i] != test)
+                i++;
+        }
         i++;
     }
+    return pass;
+}
+
+char *ft_remove_some(char *str)
+{
+
+    int i;
+    int j;
+    char *swap;
+    i = 0;
+    j = 0;
+    swap = malloc(sizeof(char)* ft_count_word(str) + 1);
+    while (str[i])
+    {
+        if (str[i] != '\'' && str[i] != '\"')
+        {
+            swap[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    swap[j] = '\0';
+    return swap;
+}
+char *split(char *str)
+{
+    int i;
+    int j;
+    char **swap;
+    char *res;
+    swap = ft_split(str,' ');
+    i = 0;
+    j = 0;
+    res = malloc(sizeof(char) * ft_sstrlen(swap) + 1);
+    while (swap[i])
+    {
+        ft_strjoin(res,ft_remove_some(swap[i]));
+        i++;
+    }
+    return res;
+}
+void lexer_main(t_data *data)
+{
+	char *swap;
+    int i = 0;
+    swap = split(data->cmd);
+    printf("%s\n",swap);
+    i++;
+
 }
 
 
-// MERHABA
 
 /*
 static int comma_pass(char const *s, int i)
@@ -86,3 +151,44 @@ char *ft_remove_some(char *str)
     return swap;
 }
 */
+
+
+/*
+static int	pass_comma(char const *s, char c, int i)
+{
+	int size;
+	size = 0;
+	while (s[i] != c && s[i])
+	{
+		size++;
+		i++;
+        if(!s[i])
+            return 0;
+	}
+	return (size);
+}
+
+static char *lexer(char *s) {
+    int i = 0;
+    int n;
+    char c;
+    int j = 0;
+    char *sub = malloc(sizeof(char) * (ft_strlen(s) + 1));
+    while (s[i]) {
+        while (s[i] == ' ')
+            i++;
+        c = s[i];
+        if (s[i] == '\'' || s[i] == '\"') {
+            n = pass_comma(s, c, i);
+            ft_strjoin(sub, ft_substr(s, i, n));
+            i += n;
+            j += n;
+            printf("%d\n",n);
+        } else {
+            while (s[i] && (s[i] != ' ' || s[i] != '\'' || s[i] != '\"'))
+                sub[j++] = s[i++];
+        }
+    }
+    sub[j] = '\0';
+    return sub;
+}*/
