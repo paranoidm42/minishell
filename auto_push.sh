d1dca18d4f96690acf2281e97d0a71fb52460c46
paranoidm42 <cemlur125@gmail.com>
2023-12-22 11:19:47 +0300

    42

--- auto_push.sh
+++ auto_push.sh
@@ -16,9 +16,6 @@ git merge main 2> /dev/null
 git add . 2> /dev/null
 git commit -m "42" 2> /dev/null
 git push > /dev/null
-merge_branch main $name "42" 2> /dev/null
-merge_branch $name main "42" 2> /dev/null
 
-
-echo $1
-echo $2
+merge_branch main "$name" "42" 2> /dev/null
+merge_branch "$name" main "42" 2> /dev/null
