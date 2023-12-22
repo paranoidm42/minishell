26b09fc373a1d421538ebd7a932ff2d018259ccc
paranoidm42 <cemlur125@gmail.com>
2023-12-22 11:02:10 +0300

    ccur

--- auto_push.sh
+++ auto_push.sh
@@ -2,12 +2,12 @@
 
 merge_branch()
 {
-    git switch "$1"
-    git pull
-    git merge "$2"
-    git add .
-    git commit -m "$3"
-    git push
+    git switch "$1" > /dev/null
+    git pull > /dev/null
+    git merge "$2" > /dev/null
+    git add . > /dev/null
+    git commit -m "$3" > /dev/null
+    git push > /dev/null
 }
 
 git merge main > /dev/null
@@ -15,5 +15,4 @@ git add . > /dev/null
 git commit -m "ccur" > /dev/null
 git push > /dev/null
 merge_branch main ccur "ccur" > /dev/null
-merge_branch ccur main "ccur" > /dev/null 
-
+merge_branch ccur main "ccur" > /dev/null
