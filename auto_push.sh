b487427e9f4b09c41ecedaffa6b3849508f7eb01
paranoidm42 <cemlur125@gmail.com>
2023-12-22 11:17:37 +0300

    ccur

--- auto_push.sh
+++ auto_push.sh
@@ -10,15 +10,13 @@ merge_branch()
     git push
 }
 
-local name="$1"
+local self="$1"
 
 git merge main 2> /dev/null
 git add . 2> /dev/null
 git commit -m "ccur" 2> /dev/null
 git push > /dev/null
-merge_branch main ccur "42" 2> /dev/null
-merge_branch ccur main "42" 2> /dev/null
+merge_branch main '$self' "42" 2> /dev/null
+merge_branch $self main "42" 2> /dev/null
 
 
-echo $1
-echo $2
