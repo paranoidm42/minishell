5bf5d7c72a1f1c0fd30cbd3bd61b86f5e4e70240
paranoidm42 <cemlur125@gmail.com>
2023-12-23 21:35:59 +0300

    42

--- auto_push.sh
+++ auto_push.sh
@@ -10,11 +10,11 @@ merge_branch()
     git push  > /dev/null
 }
 
-name="$1"  > /dev/null
+name="$1" > /dev/null 2>&1
 printf "Merhaba $name ..."
-git merge main > /dev/null
-git add . > /dev/null
-git commit -m "42" > /dev/null
+git merge main > /dev/null 2>&1
+git add . > /dev/null 2>&1
+git commit -m "42" > /dev/null 2>&1
 git push  > /dev/null
-merge_branch main "$name" "42" > /dev/null
-merge_branch "$name" main "42" > /dev/null
+merge_branch main "$name" "42" > /dev/null 2>&1
+merge_branch "$name" main "42" > /dev/null 2>&1
