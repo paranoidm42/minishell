39e4356b008286334e74cd58da93d296bce420b5
paranoidm42 <cemlur125@gmail.com>
2024-01-25 23:22:46 +0300

    42

--- srcs/executer/signals.c
+++ srcs/executer/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bcopoglu <bcopoglu@student.42kocaeli.co    +#+  +:+       +#+        */
+/*   By: ccur <ccur@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 12:03:18 by bcopoglu          #+#    #+#             */
-/*   Updated: 2024/01/25 21:37:02 by bcopoglu         ###   ########.fr       */
+/*   Updated: 2024/01/25 23:15:34 by ccur             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,42 +33,6 @@ static void	ft_interactive_handler(int32_t signum)
 		rl_on_new_line();
 		rl_redisplay();
 	}
-}
-
-static void	ft_noninteractive_handler(int32_t signum)
-{
-	if (signum == SIGINT)
-	{
-		g_signal = SIGINT;
-		ft_putchar_fd('\n', STDERR_FILENO);
-	}
-	if (signum == SIGQUIT)
-		ft_putendl_fd("Quit (core dumped)", STDIN_FILENO);
-}
-
-bool	ft_setup_noninteractive(t_init *process)
-{
-	struct termios		term;
-
-	if (tcgetattr(0, &term) == -1)
-	{
-		ft_throw_error(process, errno);
-		return (false);
-	}
-	term.c_lflag |= ECHOCTL;
-	if (tcsetattr(0, 0, &term) == -1 || \
-		signal(SIGINT, ft_noninteractive_handler) == SIG_ERR || \
-		signal(SIGQUIT, ft_noninteractive_handler) == SIG_ERR)
-	{
-		ft_throw_error(process, errno);
-		return (false);
-	}
-	if (g_signal == SIGINT)
-	{
-		process->errorcode = 130;
-		g_signal = 0;
-	}
-	return (true);
 }
 
 bool	ft_setup_interactive(t_init *process)
